45283468e56a56ee849c5698e3af7892ce0374b2
anxs <anxs@htlhomedeiMac-21.local>
2015-05-07 14:59:58 +0800

    Initial Commit

---
+++ ThirdPartyShareDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThirdPartyShareDemo
+//
+//  Created by anxs on 15/5/7.
+//  Copyright (c) 2015年 tbago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThirdPartyShareDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ThirdPartyShareDemo
+//
+//  Created by anxs on 15/5/7.
+//  Copyright (c) 2015年 tbago. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
